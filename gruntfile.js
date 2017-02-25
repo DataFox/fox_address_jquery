d36a0382d5c171ebc2e9758b283a2a64ef8242a3
Alexander Tsirel <noma4i@gmail.com>
2017-02-25 19:49:48 +1100

    initial commit

---
+++ gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+
+  var pkg = grunt.file.readJSON('package.json');
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: pkg,
+    uglify: {
+      build: {
+        src: 'src/datafox.autocomplete.js',
+        dest: 'dist/datafox.autocomplete.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+  grunt.task.registerTask('build', 'Create release', function() {
+    var version = pkg.version,
+        src = grunt.file.read('src/datafox.autocomplete.js').replace('%version%', version),
+        filePath = 'dist/datafox.autocomplete.js';
+
+    // Update not minimized release version:
+    console.log('Updating: ' + filePath);
+    grunt.file.write(filePath, src);
+
+    // Minify latest version:
+    grunt.task.run('uglify');
+
+    // Update plugin version:
+    filePath = 'datafox.autocomplete.json';
+    src = grunt.file.readJSON(filePath);
+
+    if (src.version !== version){
+      src.version = version;
+      console.log('Updating: ' + filePath);
+      grunt.file.write(filePath, JSON.stringify(src, null, 4));
+    } else {
+      console.log('No updates for: ' + filePath);
+    }
+
+    // Update bower version:
+    filePath = 'bower.json';
+    src = grunt.file.readJSON(filePath);
+
+    if (src.version !== version){
+      src.version = version;
+      console.log('Updating: ' + filePath);
+      grunt.file.write(filePath, JSON.stringify(src, null, 4));
+    } else {
+      console.log('No updates for: ' + filePath);
+    }
+  });
+};
---
+++ readme.md
@@ -0,0 +1,41 @@
+Datafox AU address autocomplete.
+
+[www.data-fox.com.au](https://data-fox.com.au/)
+
+
+##API
+
+* `$(selector).dataAddress(options);`
+    * Sets up autocomplete for input field(s).
+    * `options`: An object literal which defines the settings to use for the autocomplete plugin. Available option settings listed below.
+
+###Configuration Settings
+
+Here goes options examples
+
+## Instance Methods
+
+Autocomplete instance has following methods:
+
+* `setOptions(options)`: you may update any option at any time. Options are listed above.
+* `clear`: clears suggestion cache and current suggestions.
+* `clearCache`: clears suggestion cache.
+* `disable`: deactivate autocomplete.
+* `enable`: activates autocomplete if it was deactivated before.
+* `hide`: hides suggestions.
+* `dispose`: destroys autocomplete instance. All events are detached and suggestion containers removed.
+
+
+
+##License
+
+Based on Ajax Autocomplete for jQuery which is freely distributable under the
+terms of an MIT-style [license](https://github.com/devbridge/jQuery-Autocomplete/blob/master/dist/license.txt).
+
+Copyright notice and permission notice shall be included in all
+copies or substantial portions of the Software.
+
+##Authors
+
+Tomas Kirda / [@tkirda](https://twitter.com/tkirda)
+Alexander Tsirel / [@noma4i](https://twitter.com/noma4i)
